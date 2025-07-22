e4ce985652c5e87a8db52c333be7f31e583b6c31
Antony John <antonyjohn981@gmail.com>
2025-07-22 17:36:13 +0530

    initial spring boot commit

---
+++ src/main/java/com/example/vebnixbackend/VebnixbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.vebnixbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VebnixbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VebnixbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vebnixbackend/VebnixbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vebnixbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VebnixbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
